ab154ef491dcc484eadee220279041fcfaf48444
Habib Kalia <habibkalia@gmail.com>
2024-12-16 15:56:10 -0600

    initial project

---
+++ src/test/java/com/habibkalia/employeetracker/EmployeeTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habibkalia.employeetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
